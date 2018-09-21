6ad36b8690f4b95083bca187cdd0be0a22fbb2cb
Sujay Sanjay <mahadik203@gmail.com>
2018-09-21 15:54:19 +0530

    added SHA1 using message digest library

---
+++ ICS/Assignment3/README.md
@@ -0,0 +1,21 @@
+##  SHA1 algorithm using libraries 
+In cryptography, SHA-1 (Secure Hash Algorithm 1) is a cryptographic hash function which takes an input and produces a 160-bit (20-byte) hash value known as a message digest - typically rendered as a hexadecimal number, 40 digits long. It was designed by the United States National Security Agency, and is a U.S. Federal Information Processing Standard. [[ref]](https://en.wikipedia.org/wiki/SHA-1)
+### Sources
+[SHA-1 online](http://www.sha1-online.com/sha1-java/)
+
+Byte to Hex conversion [explanation](https://stackoverflow.com/questions/25838473/what-does-0xff-do-and-md5-structure)
+
+
+[Wiki](https://en.wikipedia.org/wiki/SHA-1)
+
+### Instructions
+
+Compile:
+
+```
+javac sha1Test.java
+```
+Run:
+```
+java sha1Test
+```
---
+++ ICS/Assignment3/sha1Test.java
@@ -0,0 +1,40 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.io.*;
+import java.util.*;
+
+public class sha1Test {
+
+    static String sha1(String ip) throws NoSuchAlgorithmException {
+
+        Scanner input = new Scanner(System.in);
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        byte[] result = md.digest(ip.getBytes());
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < result.length; i++) {
+            // byte to hexademical conversion use any one of the foll
+            // explanation at
+            // https://stackoverflow.com/questions/25838473/what-does-0xff-do-and-md5-structure
+            // sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+            // sb.append(String.format("%02x", result[i]));
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a String: ");
+            String input = scanner.nextLine();
+            System.out.println("\nInput String: " + input + " \nMessage digest using SHA1: " + sha1(input));
+
+        } catch (NoSuchAlgorithmException e) {
+            return;
+        }
+    }
+
+}
